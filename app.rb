592a94ff6e80ef08520b568212db0da7d903442d
Camilo Ordonez <icoj@gmail.com>
2015-02-11 20:06:49 -0800

    Done

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ end
 # this will store your users
 users = []
 
+
 # this will store an id to user for your users
 # you'll need to increment it every time you add
 # a new user, but don't decrement it
@@ -24,12 +25,65 @@ get '/' do
   erb :index
 end
 
+
+
 # GET /users/new - display a form for making a new user
 #
+get '/users/new' do
+  erb :create
+
+end
 # POST /users - create a user based on params from form
 #
+post '/users' do
+   @id = id
+   users.push ({name: params[:name], id: id})
+   id += 1
+   redirect to '/'
+end
 # GET /users/:id - show a user's info by their id, this should display the info in a form
 #
+get '/users/:id' do
+   users.each do |user| 
+       if params[:id].to_i == user[:id]  
+         @user = user
+       end
+   end
+  
+  erb :show
+end
+
 # PUT /users/:id - update a user's info based on the form from GET /users/:id
 #
+
+put '/users/:id' do
+   
+users.each do |user| 
+       if params[:id].to_i == user[:id]  
+         user[:name] =  params[:edited]
+       end
+   end
+redirect to '/'
+
+
+end
+
+
 # DELETE /users/:id - delete a user by their id
+
+delete '/users/:id' do
+   users.delete_if {|user| user[:id] == params[:id].to_i}
+   redirect to '/'
+end
+
+
+
+
+
+
+
+
+
+
+
+
